9358f48f9518ad387d6d1faacc026172b7c4204b
Made Rahayu <putrisaron1810@gmail.com>
2020-03-11 20:18:08 +0700

    update controller home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Item;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $items = DB::table('items')->where('stock','>',0)->get()->sortBy('name');
+        // $itemCarts = Item::has('cart')->get()->sortBy('created_at');
+
+        return view('home', ['items'=>$items]);
     }
+
+    public function dashboard()
+    {
+        return view('layouts\dashboard');
+    }
+
+   
+
 }
